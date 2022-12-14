0114781b7ba3926d89349558b37c71bcb65d13a5
DavidVer98 <davidcuevas15@hotmail.com>
2022-12-13 21:51:27 -0300

    sync urlApi

--- services/axios.ts
+++ services/axios.ts
@@ -1,7 +1,7 @@
 import Axios  from 'axios'
 
 const axiosInstance = Axios.create({
-    baseURL: 'mongodb://mongo:HuShxzrEedbhJg1KWVZT@containers-us-west-96.railway.app:5883/api',
+    baseURL: 'https://back-node-mongo-production.up.railway.app/api/',
     headers: { 'X-Custom-Header': 'foobar' }
 })
 
